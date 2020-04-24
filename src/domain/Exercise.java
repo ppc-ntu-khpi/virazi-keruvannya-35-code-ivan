aa88c1f21c3e11b71bac9ebc41b639fffdc2e8e1
Ivan <nazarenkoivan@protonmail.com>
2020-04-24 18:08:16 +0300

    Add Armstrong numbers cheking

--- src/domain/Exercise.java
+++ src/domain/Exercise.java
@@ -1,7 +1,35 @@
 package domain;
 
 public class Exercise {
-    public static float Calculate(int R){
-        return (float) (2*Math.PI*Math.sqrt(R));
+
+    /**
+     * Method that checks if your number is an Armstrong number
+     * @param number your number
+     * @return Boolean value
+     */
+    public static boolean isArmstrongNumber(int number) {
+        
+        int count = getDigitsCount(number);
+        int originalNumber = number;
+        int sum = 0;
+
+        for (int i = 0; i < count; i++) {
+            sum += Math.pow(number%10 , count);
+            number /= 10;
+        }
+
+        return (sum == originalNumber);
+    }
+
+    private static int getDigitsCount(int number) {
+
+        int count = 0;
+
+        while (number != 0) {
+            number /= 10;
+            count++;
+        }
+
+        return count;
     }
 }
